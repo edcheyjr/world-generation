c01f6e40816b9b8179e34c74aebb582a081b4fdc
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-12-22 11:40:07 +0300

    segment primitive

--- src/primitive/segment.js
+++ src/primitive/segment.js
@@ -1,4 +1,31 @@
+import Point from './point'
+
 export default class Segment {
-  constructor(p1, p2) {}
-  draw() {}
+  /**
+   * Draw joint between two points p1 and p2
+   * ______________________________________
+   *
+   * @param {Point} p1 point one
+   * @param {Point} p2 point two
+   *
+   * @return {Segment} segment class object
+   */
+  constructor(p1, p2) {
+    this.p1 = p1
+    this.p2 = p2
+  }
+  /**
+   *
+   * @param {CanvasRenderingContext2D} ctx 2D canvas context
+   * @param {number} width thickness of the line
+   * @param {string} color  line/stroke color
+   */
+  draw(ctx, width = 2, color = 'black') {
+    ctx.beginPath()
+    ctx.lineWidth = width
+    ctx.strokeStyle = color
+    ctx.moveTo(this.p1.x, this.p1.y)
+    ctx.lineTo(this.p2.x, this.p2.y)
+    ctx.stroke()
+  }
 }
