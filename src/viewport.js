c2cc1829c87ad8f0d90d3c6a7cbb1e85255639a8
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-12-26 12:23:19 +0300

    viewport class

---
+++ src/viewport.js
@@ -0,0 +1,10 @@
+export default class Viewport {
+  /**
+   * Viewport
+   * @param {HTMLCanvasElement} canvas this canvas el
+   * @returns {Viewport} new graph editor
+   */
+  constructor(canvas) {
+    this.canvas = canvas
+  }
+}
