1249959213de7fca20efcf7a361a82e636301af9
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-12-26 12:48:39 +0300

    viewport zoom

--- src/viewport.js
+++ src/viewport.js
@@ -6,5 +6,28 @@ export default class Viewport {
    */
   constructor(canvas) {
     this.canvas = canvas
+    this.zoom = 1
+    this.zoomAttir = {
+      minZoom: 1,
+      maxZoom: 6,
+      zoomSteps: 0.1,
+    }
+    this.#addEventListeners()
+  }
+  #addEventListeners() {
+    this.canvas.addEventListener('wheel', this.#handleMouseWheel.bind(this))
+  }
+  /**
+   * handle mouse wheel
+   * @param {WheelEvent} e mouse wheel event
+   */
+  #handleMouseWheel(e) {
+    const dir = Math.sign(e.deltaY) // +ve or -ve
+    this.zoom += dir * this.zoomAttir.zoomSteps
+    this.zoom = Math.max(
+      this.zoomAttir.minZoom,
+      Math.min(this.zoomAttir.maxZoom, this.zoom)
+    ) // zoom cap between min and max zooms
+    // console.log(this.zoom)
   }
 }
